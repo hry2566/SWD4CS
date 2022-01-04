cae9aa2de5f722af0ea213fb8a4bb96b1299b56b
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-01-04 10:38:15 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
-namespace SWD4CS;
-
-static class Program
+namespace SWD4CS
 {
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main()
+    internal static class Program
     {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
-        ApplicationConfiguration.Initialize();
-        Application.Run(new MainForm());
-    }    
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
 }
