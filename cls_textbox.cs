e551118b81a4d33e7b68f27ee8eb309d4b596e30
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-01-04 10:37:56 +0900

    Update cls_textbox.cs

--- cls_textbox.cs
+++ cls_textbox.cs
@@ -5,14 +5,14 @@ namespace SWD4CS
     {
         public cls_control_base ctrlBase;
 
-        public cls_textbox(Control parent, Control backPanel, int index, int X = 0, int Y = 0)
+        public cls_textbox(Control parent, Control backPanel, DataGridView propertyList, int index, int X = 0, int Y = 0)
         {
             InitializeComponent(index, X, Y);
 
             cls_form ctrl = (cls_form)parent;
             ctrl.CtrlItems.Add(this);
-            ctrl.Controls.Add((cls_textbox)ctrl.CtrlItems[ctrl.CtrlItems.Count - 1]);
-            ctrlBase = new cls_control_base(parent, this, backPanel);
+            ctrl.Controls.Add(ctrl.CtrlItems[ctrl.CtrlItems.Count - 1] as cls_textbox as cls_textbox);
+            ctrlBase = new cls_control_base(parent, this, backPanel, propertyList);
         }
 
         public void Deleate(Control parent, cls_textbox ctrl)
@@ -21,6 +21,5 @@ namespace SWD4CS
             ctrlBase.SetSelect(false);
             this.Dispose();
         }
-
     }
 }
