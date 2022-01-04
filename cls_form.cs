475ae5713c44e637eecfe6909cf2c92f6fae0685
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-01-04 10:37:34 +0900

    Update cls_form.cs

--- cls_form.cs
+++ cls_form.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Collections;
+using System.Data;
 
 namespace SWD4CS
 {
@@ -9,30 +10,100 @@ namespace SWD4CS
         private cls_selectbox selectBox;
         private Control backPanel;
         private ListBox toolList;
+        private DataGridView propertyList;
         private bool selectFlag = false;
         private int grid = 8;
         private int cnt_Button;
         private int cnt_Label;
         private int cnt_TextBox;
-        
+        private int cnt_ListBox;
 
         public cls_form()
         {
             InitializeComponent();
         }
 
-        public void Init(Control backPanel, ListBox toolList)
+        public void Init(Control backPanel, ListBox toolList, DataGridView dataGridView1)
         {
             this.backPanel = backPanel;
             this.toolList = toolList;
+            this.propertyList = dataGridView1;
 
             backPanel.Click += new System.EventHandler(Backpanel_Click);
             this.Click += new System.EventHandler(Form_Click);
+            this.propertyList.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+
 
             selectBox = new cls_selectbox(this, backPanel);
         }
 
-        private void Backpanel_Click(object sender, EventArgs e)
+        private void SetProperty(int i, int index, bool formFlag)
+        {
+            string? propertyName = "";
+            string? propertyValue = "";
+
+            if (propertyList.Rows[index].Cells[0].Value == null)
+            {
+                return;
+            }
+            else
+            {
+                propertyName = propertyList.Rows[index].Cells[0].Value.ToString();
+            }
+
+            if (propertyList.Rows[index].Cells[1].Value != null)
+            {
+                propertyValue = propertyList.Rows[index].Cells[1].Value.ToString();
+            }
+
+            if (formFlag)
+            {
+                if (propertyName == "Size.Width")
+                {
+                    this.Width = Int32.Parse(propertyValue);
+                }
+                else if (propertyName == "Size.Height")
+                {
+                    this.Height = Int32.Parse(propertyValue);
+                }
+                else if (propertyName == "Text")
+                {
+                    this.Text = propertyValue;
+                }
+                return;
+            }
+            else
+            {
+                Control? item = CtrlItems[i] as Control;
+
+                if (propertyName == "Name")
+                {
+                    item.Name = propertyValue;
+                }
+                else if (propertyName == "Location.X")
+                {
+                    item.Left = Int32.Parse(propertyValue);
+                }
+                else if (propertyName == "Location.Y")
+                {
+                    item.Top = Int32.Parse(propertyValue);
+                }
+                else if (propertyName == "Size.Width")
+                {
+                    item.Width = Int32.Parse(propertyValue);
+                }
+                else if (propertyName == "Size.Height")
+                {
+                    item.Height = Int32.Parse(propertyValue);
+                }
+                else if (propertyName == "Text")
+                {
+                    item.Text = propertyValue;
+                }
+            }
+        }
+
+        private void Backpanel_Click(object? sender, EventArgs e)
         {
             MouseEventArgs me = (MouseEventArgs)e;
 
@@ -49,9 +120,24 @@ namespace SWD4CS
         {
             selectFlag = flag;
             selectBox.SetSelectBoxPos(selectFlag);
+
+            // 選択されていたらプロパティ表示
+            if (flag)
+            {
+                propertyList.Rows.Clear();
+                propertyList.Rows.Add("Name", "From1");
+                propertyList.Rows.Add("Size.Width", this.Size.Width);
+                propertyList.Rows.Add("Size.Height", this.Size.Height);
+                propertyList.Rows.Add("TabIndex", this.TabIndex);
+                propertyList.Rows.Add("Text", this.Text);
+            }
+            else
+            {
+                propertyList.Rows.Clear();
+            }
         }
 
-        private void Form_Click(object sender, EventArgs e)
+        private void Form_Click(object? sender, EventArgs e)
         {
             if (toolList.Text == "")
             {
@@ -61,7 +147,6 @@ namespace SWD4CS
                 {
                     if (selectFlag)
                     {
-                        SelectAllClear();
                         SetSelect(false);
                     }
                     else
@@ -75,100 +160,194 @@ namespace SWD4CS
             {
                 MouseEventArgs me = (MouseEventArgs)e;
 
-                // Add Button
+                // unselect
+                SelectAllClear();
+
+                // Add 
                 int X = (int)(me.X / grid) * grid;
                 int Y = (int)(me.Y / grid) * grid;
 
-                if (toolList.Text == "Button")
-                {
-                    cls_button ctrl = new cls_button(this, backPanel, cnt_Button, X, Y);
-                    cnt_Button++;
-                }
-                else if (toolList.Text == "Label")
-                {
-                    cls_label ctrl = new cls_label(this, backPanel, cnt_Label, X, Y);
-                    cnt_Label++;
-                }
-                else if (toolList.Text == "TextBox")
-                {
-                    cls_textbox ctrl = new cls_textbox(this, backPanel, cnt_TextBox, X, Y);
-                    cnt_TextBox++;
-                }
-
-                // unselect
-                SelectAllClear(true);
+                AddControl(X, Y);
 
                 toolList.SelectedIndex = -1;
-
-                SetSelect(false);
             }
 
         }
 
-        private void SelectAllClear(bool flag = false)
+        internal void SelectAllClear()
         {
-            int cnt = 0;
+            SetSelect(false);
 
-            if (flag)
+            for (int i = 0; i < CtrlItems.Count; i++)
             {
-                cnt = 1;
-            }
-
-            for (int i = 0; i < CtrlItems.Count - cnt; i++)
-            {
-                if (CtrlItems[i].GetType().ToString() == "SWD4CS.cls_button")
-                {
-                    cls_button ctrl = (cls_button)CtrlItems[i];
-                    ctrl.ctrlBase.SetSelect(false);
-                }
-                else if (CtrlItems[i].GetType().ToString() == "SWD4CS.cls_label")
-                {
-                    cls_label ctrl = (cls_label)CtrlItems[i];
-                    ctrl.ctrlBase.SetSelect(false);
-                }
-                else if (CtrlItems[i].GetType().ToString() == "SWD4CS.cls_textbox")
-                {
-                    cls_textbox ctrl = (cls_textbox)CtrlItems[i];
-                    ctrl.ctrlBase.SetSelect(false);
-                }
+                ControlCom(i, 0, 0);
             }
         }
 
-        public void RemoveSelectedItem()
+        internal void RemoveSelectedItem()
         {
             for (int i = 0; i < CtrlItems.Count; i++)
             {
-                if (CtrlItems[i].GetType().ToString() == "SWD4CS.cls_button")
+                if (ControlCom(i, 1, 0))
                 {
-                    cls_button ctrl = (cls_button)CtrlItems[i];
-                    if (ctrl.ctrlBase.GetSelected())
-                    {
-                        ctrl.Deleate(this, ctrl);
-                        CtrlItems.Remove(ctrl);
-                        i = -1;
-                    }
+                    i--;
                 }
-                else if (CtrlItems[i].GetType().ToString() == "SWD4CS.cls_label")
+            }
+        }
+
+        private void dataGridView1_CellValueChanged(object? sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;
+
+            if (this.selectFlag)
+            {
+                SetProperty(0, index, true);
+                SetSelect(true);
+            }
+            else
+            {
+                for (int i = 0; i < CtrlItems.Count; i++)
                 {
-                    cls_label ctrl = (cls_label)CtrlItems[i];
-                    if (ctrl.ctrlBase.GetSelected())
+                    if (ControlCom(i, 2, index))
                     {
-                        ctrl.Deleate(this, ctrl);
-                        CtrlItems.Remove(ctrl);
-                        i = -1;
-                    }
-                }
-                else if (CtrlItems[i].GetType().ToString() == "SWD4CS.cls_textbox")
-                {
-                    cls_textbox ctrl = (cls_textbox)CtrlItems[i];
-                    if (ctrl.ctrlBase.GetSelected())
-                    {
-                        ctrl.Deleate(this, ctrl);
-                        CtrlItems.Remove(ctrl);
-                        i = -1;
+                        break;
                     }
                 }
             }
         }
+
+        // ****************************************************************************************
+        // コントロール追加時に下記を編集すること
+        // ****************************************************************************************
+        private void AddControl(int X, int Y)
+        {
+            if (toolList.Text == "Button")
+            {
+                cls_button ctrl = new cls_button(this, backPanel, propertyList, cnt_Button, X, Y);
+                cnt_Button++;
+            }
+            else if (toolList.Text == "Label")
+            {
+                cls_label ctrl = new cls_label(this, backPanel, propertyList, cnt_Label, X, Y);
+                cnt_Label++;
+            }
+            else if (toolList.Text == "TextBox")
+            {
+                cls_textbox ctrl = new cls_textbox(this, backPanel, propertyList, cnt_TextBox, X, Y);
+                cnt_TextBox++;
+            }
+            else if (toolList.Text == "ListBox")
+            {
+                cls_listbox ctrl = new cls_listbox(this, backPanel, propertyList, cnt_TextBox, X, Y);
+                cnt_ListBox++;
+            }
+        }
+        
+        private bool ControlCom(int i, int mode, int index)
+        {
+            if (CtrlItems[i] is cls_button)
+            {
+                cls_button? ctrl = CtrlItems[i] as cls_button;
+
+                if (mode == 0)
+                {
+                    ctrl.ctrlBase.SetSelect(false);
+                }
+                else
+                {
+                    if (ctrl.ctrlBase.GetSelected())
+                    {
+                        if (mode == 1)
+                        {
+                            ctrl.Deleate(this, ctrl);
+                            CtrlItems.Remove(ctrl);
+                        }
+                        else if (mode == 2)
+                        {
+                            SetProperty(i, index, false);
+                            ctrl.ctrlBase.SetSelect(true);
+                        }
+                        return true;
+                    }
+                }
+            }
+            else if (CtrlItems[i] is cls_label)
+            {
+                cls_label? ctrl = CtrlItems[i] as cls_label;
+                if (mode == 0)
+                {
+                    ctrl.ctrlBase.SetSelect(false);
+                }
+                else
+                {
+                    if (ctrl.ctrlBase.GetSelected())
+                    {
+                        if (mode == 1)
+                        {
+                            ctrl.Deleate(this, ctrl);
+                            CtrlItems.Remove(ctrl);
+                        }
+                        else if (mode == 2)
+                        {
+                            SetProperty(i, index, false);
+                            ctrl.ctrlBase.SetSelect(true);
+                        }
+                        return true;
+                    }
+                }
+            }
+            else if (CtrlItems[i] is cls_textbox)
+            {
+                cls_textbox? ctrl = CtrlItems[i] as cls_textbox;
+                if (mode == 0)
+                {
+                    ctrl.ctrlBase.SetSelect(false);
+                }
+                else
+                {
+                    if (ctrl.ctrlBase.GetSelected())
+                    {
+                        if (mode == 1)
+                        {
+                            ctrl.Deleate(this, ctrl);
+                            CtrlItems.Remove(ctrl);
+                        }
+                        else if (mode == 2)
+                        {
+                            SetProperty(i, index, false);
+                            ctrl.ctrlBase.SetSelect(true);
+                        }
+                        return true;
+                    }
+                }
+            }
+            else if (CtrlItems[i] is cls_listbox)
+            {
+                cls_listbox? ctrl = CtrlItems[i] as cls_listbox;
+                if (mode == 0)
+                {
+                    ctrl.ctrlBase.SetSelect(false);
+                }
+                else
+                {
+                    if (ctrl.ctrlBase.GetSelected())
+                    {
+                        if (mode == 1)
+                        {
+                            ctrl.Deleate(this, ctrl);
+                            CtrlItems.Remove(ctrl);
+                        }
+                        else if (mode == 2)
+                        {
+                            SetProperty(i, index, false);
+                            ctrl.ctrlBase.SetSelect(true);
+                        }
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        // ****************************************************************************************
     }
 }
