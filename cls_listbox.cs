97145d36d10eab122aca6f26d68c5eb3c5b0cb46
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-01-04 10:37:45 +0900

    Create cls_listbox.cs

---
+++ cls_listbox.cs
@@ -0,0 +1,25 @@
+﻿
+namespace SWD4CS
+{
+    public partial class cls_listbox : ListBox
+    {
+        public cls_control_base ctrlBase;
+
+        public cls_listbox(Control parent, Control backPanel, DataGridView propertyList, int index, int X = 0, int Y = 0)
+        {
+            InitializeComponent(index, X, Y);
+
+            cls_form ctrl = (cls_form)parent;
+            ctrl.CtrlItems.Add(this);
+            ctrl.Controls.Add(ctrl.CtrlItems[ctrl.CtrlItems.Count - 1] as cls_listbox);
+            ctrlBase = new cls_control_base(parent, this, backPanel, propertyList);
+        }
+
+        public void Deleate(Control parent, cls_listbox ctrl)
+        {
+            parent.Controls.Remove(ctrl);
+            ctrlBase.SetSelect(false);
+            this.Dispose();
+        }
+    }
+}
