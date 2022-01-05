86184688885beb2bfa5f4be1cf199464650abf53
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-01-05 11:19:01 +0900

    Update cls_label.cs

--- cls_label.cs
+++ cls_label.cs
@@ -3,16 +3,19 @@ namespace SWD4CS
 {
     public partial class cls_label : Label
     {
-        public cls_control_base ctrlBase;
+        internal cls_control_base ctrlBase;
+        private cls_form form;
+        internal Control parentCtrl;
 
-        public cls_label(Control parent, Control backPanel, DataGridView propertyList, int index, int X = 0, int Y = 0)
+        public cls_label(cls_form form, Control parent, Control backPanel, ListBox toolList, DataGridView propertyList, int index, int X = 0, int Y = 0)
         {
             InitializeComponent(index, X, Y);
 
-            cls_form ctrl = (cls_form)parent;
-            ctrl.CtrlItems.Add(this);
-            ctrl.Controls.Add(ctrl.CtrlItems[ctrl.CtrlItems.Count - 1] as cls_label);
-            ctrlBase = new cls_control_base(parent, this, backPanel, propertyList);
+            this.form = form;
+            this.parentCtrl = parent;
+            this.form.CtrlItems!.Add(this);
+            parent.Controls.Add(this.form.CtrlItems[this.form.CtrlItems.Count - 1] as cls_label);
+            ctrlBase = new cls_control_base(form, this, parent, backPanel, propertyList);
         }
 
         public void Deleate(Control parent, cls_label ctrl)
