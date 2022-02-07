a484b4b4b53125717dbe1a4b645d190d722a2e6c
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-02-07 21:00:46 +0900

    Create cls_treenode.cs

---
+++ cls_treenode.cs
@@ -0,0 +1,39 @@
+﻿
+namespace SWD4CS
+{
+    internal class cls_treenode : TreeNode
+    {
+        private cls_treenode[] itemNode = Array.Empty<cls_treenode>();
+
+        public cls_treenode(string nodeName)
+        {
+            this.Text = nodeName;
+        }
+
+        internal cls_treenode? Search(string name)
+        {
+            if (Text == name)
+            {
+                return this;
+            }
+
+            for (int i = 0; i < itemNode.Count(); i++)
+            {
+                if (itemNode[i].Search(name) != null)
+                {
+                    return itemNode[i].Search(name);
+                }
+            }
+            return null;
+        }
+
+        internal void Add(string name)
+        {
+            Array.Resize(ref itemNode, itemNode.Count() + 1);
+            itemNode[itemNode.Count() - 1] = new cls_treenode(name);
+            this.Nodes.Clear();
+            this.Nodes.AddRange(itemNode);
+            this.Expand();
+        }
+    }
+}
