2281b8305d0b318b399ceec7c2cab7057d6a58ab
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-03-26 19:39:46 +0900

    Create cls_user_datagridview.cs

---
+++ cls_user_datagridview.cs
@@ -0,0 +1,203 @@
+﻿using System.Data;
+using System.Reflection;
+
+namespace SWD4CS
+{
+    public partial class cls_user_datagridview : DataGridView
+    {
+        private cls_userform? form;
+        private cls_controls? cls_ctrl;
+        public cls_user_datagridview()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            this.AllowUserToAddRows = false;
+            this.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(cls_user_datagridview1_CellMouseDoubleClick);
+        }
+
+        internal void ShowEventList(bool flag, cls_userform form)
+        {
+            this.form = form;
+            this.cls_ctrl = null;
+
+            List<string> evnt = new();
+            List<string> fnc = new();
+            string[] split;
+            for (int i = 0; i < form.decHandler.Count; i++)
+            {
+                split = form.decHandler[i].Split("+=")[0].Split(".");
+                evnt.Add(split[split.Length - 1].Trim());
+
+                split = form.decFunc[i].Split("(")[0].Split(" ");
+                fnc.Add(split[split.Length - 1].Trim());
+            }
+
+            SetEventsData(flag, form.memForm, evnt, fnc);
+        }
+        internal void ShowEventList(bool flag, cls_controls ctrl)
+        {
+            this.form = null;
+            this.cls_ctrl = ctrl;
+
+            List<string> evnt = new();
+            List<string> fnc = new();
+            string[] split;
+            for (int i = 0; i < ctrl.decHandler.Count; i++)
+            {
+                split = ctrl.decHandler[i].Split("+=")[0].Split(".");
+                evnt.Add(split[split.Length - 1].Trim());
+
+                split = ctrl.decFunc[i].Split("(")[0].Split(" ");
+                fnc.Add(split[split.Length - 1].Trim());
+            }
+            SetEventsData(flag, ctrl.ctrl, evnt, fnc);
+        }
+
+        private void SetEventsData(bool flag, Control? ctrl, List<string> evnt, List<string> fnc)
+        {
+            DataTable table = new();
+
+            table.Columns.Add("Event");
+            table.Columns.Add("Function");
+
+            if (flag)
+            {
+                Type type = ctrl!.GetType();
+                MemberInfo[] members = type.GetMembers();
+                foreach (MemberInfo m in members)
+                {
+                    string funcName = "";
+                    if (m.MemberType.ToString() == "Event")
+                    {
+                        for (int i = 0; i < evnt.Count; i++)
+                        {
+                            if (evnt[i] == m.Name)
+                            {
+                                funcName = fnc[i];
+                                break;
+                            }
+                        }
+                        table.Rows.Add(m.Name, funcName);
+                    }
+                }
+            }
+
+            this.DataSource = table;
+            this.Sort(this.Columns[0], System.ComponentModel.ListSortDirection.Ascending);
+            this.Columns[0].ReadOnly = true;
+            this.Columns[1].ReadOnly = true;
+            this.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            this.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
+        private void cls_user_datagridview1_CellMouseDoubleClick(object? sender, DataGridViewCellMouseEventArgs e)
+        {
+            Control? ctrl;
+
+            if (this.form != null)
+            {
+                ctrl = this.form.memForm;
+            }
+            else
+            {
+                ctrl = this.cls_ctrl!.ctrl;
+            }
+
+            string? eventName = this.Rows[e.RowIndex].Cells[0].Value.ToString();
+            string? newHandler;
+            string? funcName = ctrl!.Name + "_" + eventName;
+            string funcParam = "";
+
+
+            if (this.Rows[e.RowIndex].Cells[1].Value.ToString() == "")
+            {
+                this.Rows[e.RowIndex].Cells[1].Value = funcName;
+
+                Type? delegateType = ctrl.GetType().GetEvent(eventName!)!.EventHandlerType;
+                MethodInfo? invoke = delegateType!.GetMethod("Invoke");
+                ParameterInfo[] pars = invoke!.GetParameters();
+                string[] split = delegateType.AssemblyQualifiedName!.Split(",");
+                newHandler = "new " + split[0];
+
+                foreach (ParameterInfo p in pars)
+                {
+                    string param = p.ParameterType.ToString();
+
+                    if (param == "System.Object")
+                    {
+                        param += "? sender";
+                    }
+                    else
+                    {
+                        param += " e";
+                    }
+
+                    if (funcParam == "")
+                    {
+                        funcParam = param;
+                    }
+                    else
+                    {
+                        funcParam += ", " + param;
+                    }
+                }
+
+                string decHandler;
+                if (this.form != null)
+                {
+                    decHandler = "this." + eventName + " += " + newHandler + "(" + funcName + ");";
+
+                }
+                else
+                {
+                    decHandler = "this." + ctrl.Name + "." + eventName + " += " + newHandler + "(" + funcName + ");";
+
+                }
+                string decFunc = "private void " + funcName + "(" + funcParam + ")";
+
+                if (this.form != null)
+                {
+                    this.form!.decHandler.Add(decHandler);
+                    this.form.decFunc.Add(decFunc);
+                }
+                else
+                {
+                    this.cls_ctrl!.decHandler.Add(decHandler);
+                    this.cls_ctrl.decFunc.Add(decFunc);
+                }
+            }
+            else
+            {
+                this.Rows[e.RowIndex].Cells[1].Value = "";
+
+                if (this.form != null)
+                {
+                    for (int i = 0; i < this.form!.decFunc.Count; i++)
+                    {
+                        string[] split = this.form!.decFunc[i].Split("(")[0].Split(" ");
+
+                        if (split[split.Length - 1] == funcName)
+                        {
+                            this.form!.decHandler.Remove(this.form!.decHandler[i]);
+                            this.form!.decFunc.Remove(this.form!.decFunc[i]);
+                            break;
+                        }
+                    }
+                }
+                else if (this.cls_ctrl != null)
+                {
+                    for (int i = 0; i < this.cls_ctrl!.decFunc.Count; i++)
+                    {
+                        string[] split = this.cls_ctrl!.decFunc[i].Split("(")[0].Split(" ");
+
+                        if (split[split.Length - 1] == funcName)
+                        {
+                            this.cls_ctrl!.decHandler.Remove(this.cls_ctrl!.decHandler[i]);
+                            this.cls_ctrl!.decFunc.Remove(this.cls_ctrl!.decFunc[i]);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
