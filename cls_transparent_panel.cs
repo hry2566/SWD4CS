d9342098b2fc645539bff61ba6725de8d21c83d8
hry2566 <86605611+hry2566@users.noreply.github.com>
2023-01-18 21:25:45 +0900

    Create cls_transparent_panel.cs

---
+++ cls_transparent_panel.cs
@@ -0,0 +1,30 @@
+using System.Reflection;
+
+namespace SWD4CS
+{
+    public class cls_transparent_panel : Panel
+    {
+        public cls_transparent_panel()
+        {
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, false);
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                var result = base.CreateParams;
+                result.ExStyle |= 0x20;
+                return result;
+            }
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            using (var brush = new SolidBrush(BackColor))
+            {
+                e.Graphics.FillRectangle(brush, e.ClipRectangle);
+            }
+        }
+    }
+}
